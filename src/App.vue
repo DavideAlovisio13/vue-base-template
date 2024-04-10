0d88caf30953a3e4273795290c1390756db6f723
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-10 10:59:31 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-center">La Molisana</h1>
+    <i class="fas fa-spinner fa-pulse"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
